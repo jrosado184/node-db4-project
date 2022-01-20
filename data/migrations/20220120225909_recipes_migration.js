1ea147e6663c7fbdbcdc58aef0fc065a0b59f5bb
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-20 18:10:59 -0500

    added constraint to recipe table

--- data/migrations/20220120225909_recipes_migration.js
+++ data/migrations/20220120225909_recipes_migration.js
@@ -2,7 +2,7 @@ exports.up = async function (knex) {
   await knex.schema
     .createTable("recipes", (table) => {
       table.increments("recipe_id");
-      table.text("recipe_name", 128);
+      table.text("recipe_name", 128).unique().notNullable();
     })
     .createTable("steps", (table) => {
       table.increments("step_id");
